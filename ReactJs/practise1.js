6d8fc164f4693ce540b7d63a319c0c23a5ba41eb
siddarth04s <158273994+siddarth04s@users.noreply.github.com>
2024-03-04 18:34:42 +0530

    Add files via upload

---
+++ ReactJs/practise1.js
@@ -0,0 +1,10 @@
+let fruit = "apple"
+
+// ? :  --> if else   
+// === and &&  -> condition check
+const sol = fruit=="apple" && "welcome"
+console.log(sol)
+
+const comp =() =>{
+    return fruit==apple?<h1>hii all</h1>:<h1>hello all</h1>
+}
